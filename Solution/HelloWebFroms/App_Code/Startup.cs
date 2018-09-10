dba630bb0e49b3307a2bfecb176425993ea6335b
Joshua Sereno <serenojoshua@accenture.com>
2018-09-10 11:56:56 -0500

    Initial Commit

---
+++ Solution/HelloWebFroms/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWebFroms.Startup))]
+namespace HelloWebFroms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
