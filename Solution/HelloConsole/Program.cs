2a415f761eba30462a4d54a1ebbc95a5c9e65899
spusulri <43144724+spusulri@users.noreply.github.com>
2018-09-10 12:01:26 -0500

    Update Program.cs

--- Solution/HelloConsole/Program.cs
+++ Solution/HelloConsole/Program.cs
@@ -11,7 +11,7 @@ namespace HelloConsole
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello NFCU");
             Console.ReadLine();
         }
     }
